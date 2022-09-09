7c4dbf1735d17a8aff92de75f249f776efe4ca56
Mohcine SEDOUANE <MouhcineS@algoconsulting.net>
2022-09-09 01:15:05 +0100

    change config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // images: {
-  //   loader: 'imgix',
-  //   path: '/',
-  //   },
+   images: {
+     loader: 'imgix',
+     path: '/',
+     },
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
 
